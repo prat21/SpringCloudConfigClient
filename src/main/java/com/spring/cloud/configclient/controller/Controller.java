111fd897bb6bf3ea4ba42d90d2f1e34baabd7ad1
pratik <prmukherjee.21@gmail.com>
2019-07-24 00:34:12 +0530

    Initial commit

---
+++ src/main/java/com/spring/cloud/configclient/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@Value("${user.role:default role value---}")
+	String role;
+	
+	@GetMapping("/test")
+	public void test(){
+		System.out.println("user.role: "+role);
+	}
+}
